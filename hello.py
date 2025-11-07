abde3cc0a1717237df08d02214e4ecc0bd116489
kiruthikpurpose <kiruthikpurpose@gmail.com>
2025-11-07 12:18:33 +0530

    Created a file

---
+++ hello.py
@@ -0,0 +1,9 @@
+def find_largest_number(numbers):
+    if not numbers:
+        return None  # Handle empty list case
+    
+    largest = numbers[0]
+    for number in numbers:
+        if number > largest:
+            largest = number
+    return largest
